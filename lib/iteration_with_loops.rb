2f2390ebae0eabb64078d18ba45211ccfadfc33b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-01 19:22:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
 row_index = 0
 while row_index < src.count do
  element_index = 0
- new_string = ""
+ new_string = " "
  while element_index < src[row_index].count do
    if src[row_index][element_index].is_a?(String)
      new_string += src[row_index][element_index]
